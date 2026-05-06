8618daf128193982fa853d0491101c35fdcc4253
KRUSTstation <115158641+KRUSTstation@users.noreply.github.com>
2026-05-06 17:00:59 +0800

    design changes

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE Queue (
     queue_number INT NOT NULL,
     ph_num VARCHAR(20) NOT NULL,
     name VARCHAR(100) NOT NULL,
+    attempts INT NOT NULL DEFAULT 1,
     status TEXT NOT NULL DEFAULT 'waiting' CHECK(status IN ('waiting', 'serving', 'done')),
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP UNIQUE
 );
